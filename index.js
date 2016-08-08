f98aba68e8cdc118f24a786eaf38e5935bf4391e
Ari Lotter <ari.lotter@jam3.com>
2016-08-08 18:29:58 -0400

    got MSDF generation working!

--- index.js
+++ index.js
@@ -4,18 +4,23 @@ const path = require('path');
 
 const makeSDF = getSDFGenerator('Roboto-Regular.ttf');
 
-const charset = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
-// const charset = 'abcdefghijklmnop';
+const charset = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZ z';
+// const charset = 'j';
 const fontSize = 32;
-const pageWidth = 256;
-const pageHeight = 256;
+const pageWidth = 512;
+const pageHeight = 512;
 const pagePadding = 2;
 
 makeSDF(charset, fontSize, pageWidth, pageHeight, pagePadding, (generated) => {
   generated.spritesheets.forEach((sheet, index) => {
+    generated.fontData.pages.push(`sheet${index}.png`);
     fs.writeFile(path.join('output', `sheet${index}.png`), sheet, (err) => {
       if (err) throw err;
       console.log('wrote spritesheet', index);
     });
+  });
+  fs.writeFile(path.join('output', 'font.fnt'), JSON.stringify(generated.fontData), (err) => {
+    if (err) throw err;
+    console.log('wrote font file');
   });
 });
--- msdfgen.js
+++ msdfgen.js
@@ -6,14 +6,15 @@ const Canvas = require('canvas');
 
 module.exports = function (fontPath) {
   const font = opentype.loadSync(fontPath);
-  return generateSpritesheet;
+  return generateBMFont;
 
-  function generateSpritesheet (charset, fontSize, width, height, padding, callback) { // convert this to opts obj
+  function generateBMFont (charset, fontSize, width, height, padding, callback) { // convert this to opts obj
     const canvas = new Canvas(width, height);
     const context = canvas.getContext('2d');
     const packer = new MultiBinPacker(width, height, padding);
+    const chars = [];
     mapLimit(charset.split(''), 15, (char, cb) => {
-      generateImage(char, fontSize, (err, res) => {
+      generateImage(char, fontSize, 'msdf', (err, res) => {
         if (err) return cb(err);
         cb(null, res);
       });
@@ -23,18 +24,54 @@ module.exports = function (fontPath) {
       const spritesheets = packer.bins.map((bin, index) => {
         context.clearRect(0, 0, canvas.width, canvas.height);
         bin.rects.forEach(rect => {
-          context.putImageData(rect.data, rect.x, rect.y);
+          if (rect.data.imageData) {
+            context.putImageData(rect.data.imageData, rect.x, rect.y);
+          }
+          const charData = rect.data.fontData;
+          charData.x = rect.x;
+          charData.y = rect.y;
+          charData.page = index;
+          chars.push(rect.data.fontData);
         });
         return canvas.toBuffer();
       });
-      callback({spritesheets});
+      const os2 = font.tables.os2;
+      const fontData = {
+        pages: [],
+        chars,
+        info: {
+          face: `${font.familyName} ${font.styleName}`,
+          size: fontSize,
+          bold: 0,
+          italic: 0,
+          charset,
+          unicode: 1,
+          stretchH: 100,
+          smooth: 1,
+          aa: 1,
+          padding: [0, 0, 0, 0],
+          spacing: [padding, padding]
+        },
+        common: {
+          lineHeight: os2.sTypoAscender - os2.sTypoDescender + os2.sTypoLineGap,
+          base: font.ascender * (fontSize / font.unitsPerEm),
+          scaleW: width,
+          scaleH: height,
+          pages: packer.bins.length,
+          packed: 0,
+          alphaChnl: 0,
+          redChnl: 0,
+          greenChnl: 0,
+          blueChnl: 0
+        }
+      };
+      callback({spritesheets, fontData});
     });
   }
 
-  function generateImage (char, fontSize, callback) {
+  function generateImage (char, fontSize, fieldType, callback) {
     const glyph = font.charToGlyph(char);
     const commands = glyph.getPath(0, 0, fontSize).commands;
-
     let contours = [];
     let currentContour = [];
     commands.forEach(command => {
@@ -54,7 +91,8 @@ module.exports = function (fontPath) {
       const lastIndex = contour.length - 1;
       contour.forEach((command, index) => {
         if (command.type === 'Z') {
-          shapeDesc += '#';
+          // shapeDesc += `${contour[0].x}, ${contour[0].y}`;
+          // adding the last point breaks it??!??!
         } else {
           // make sure to invert y coordinates
           if (command.type === 'C') {
@@ -73,36 +111,52 @@ module.exports = function (fontPath) {
 
     if (contours.some(cont => cont.length === 1)) console.log('length is 1, failed to normalize glyph');
     const scale = fontSize / font.unitsPerEm;
-    // const width = Math.round((glyph.xMax - glyph.xMin) * scale) + 4;
-    // const height = Math.round((glyph.yMax - glyph.yMin) * scale) + 4;
-    // let command = `./msdfgen.bin msdf -format text -stdout -size ${width} ${height} -scale ${scale} -translate -2 2  -defineshape "${shapeDesc}"`;
-    // command += ` -testrender output/${char.charCodeAt(0)}-render.png 1024 1024`;
-    const pad = 3;
-    const width = Math.round((glyph.xMax - glyph.xMin) * scale) + pad + pad;
-    const height = Math.round((glyph.yMax - glyph.yMin) * scale) + pad + pad;
-    let command = `./msdfgen.bin msdf -format text -stdout -size ${width} ${height} -translate 1 ${height - 4} -pxrange 3 -defineshape "${shapeDesc}"`;
-    command += ` -testrender output/${char.charCodeAt(0)}-render.png ${width * 10} ${height * 10}`;
-    if (char === 'j') {
-      console.log('width', glyph.xMax - glyph.xMin);
-      console.log('scaledWidth', width - 4);
-
-      console.log('height', glyph.yMax - glyph.yMin);
-      console.log('scaledHeight', height - 4);
-    }
+    const pad = 5;
+    let width = Math.round((glyph.xMax - glyph.xMin) * scale) + pad + pad;
+    let height = Math.round((font.ascender - font.descender) * scale) + pad + pad;
+    // const height = Math.round((glyph.yMax - glyph.yMin) * scale) + pad + pad;
+    let command = `./msdfgen.bin ${fieldType} -format text -stdout -size ${width} ${height} -translate ${pad} ${font.ascender * scale} -pxrange 3 -defineshape "${shapeDesc}"`;
+    // command += ` -testrender output/${char.charCodeAt(0)}-render.png ${width * 10} ${height * 10}`;
     exec(command, (err, stdout, stderr) => {
       if (err) callback(err);
       const rawImageData = stdout.match(/([0-9a-fA-F]+)/g).map(str => parseInt(str, 16)); // split on every number, parse
       const pixels = [];
-      for (let i = 0; i < rawImageData.length; i += 3) {
-        pixels.push(...rawImageData.slice(i, i + 3), 255); // add 255 as alpha every 3 elements
+      if (fieldType === 'sdf') {
+        for (let i = 0; i < rawImageData.length; i++) {
+          pixels.push(rawImageData[i], rawImageData[i], rawImageData[i], 255); // add 255 as alpha every 3 elements
+        }
+      } else if (fieldType === 'msdf') {
+        for (let i = 0; i < rawImageData.length; i += 3) {
+          pixels.push(...rawImageData.slice(i, i + 3), 255); // add 255 as alpha every 3 elements
+        }
       }
-      if (((pixels.length / 4) / width) / height !== 1) {
-        console.log('output error, image is wrong length');
-        console.log('expected', width * height * 4, 'got', pixels.length);
-        debugger;
+      // if (((pixels.length / 4) / width) / height !== 1) {
+      //   console.log('output error, image is wrong length');
+      //   console.log('expected', width * height * 4, 'got', pixels.length);
+      //   debugger;
+      // }
+      let imageData;
+      try {
+        imageData = new Canvas.ImageData(new Uint8ClampedArray(pixels), width, height);
+      } catch (err) {
+        console.log(`failed to generate image for character '${char}' (character code ${char.charCodeAt(0)})`);
+        width = 0;
+        height = 0;
       }
-      const imageData = new Canvas.ImageData(new Uint8ClampedArray(pixels), width, height);
-      const container = {data: imageData, width, height};
+      const container = {
+        data: {
+          imageData,
+          fontData: {
+            id: char.charCodeAt(0),
+            width, height,
+            xoffset: 0,
+            yoffset: 0,
+            xadvance: glyph.advanceWidth * scale,
+            chnl: 15
+          }
+        },
+        width, height
+      };
       callback(null, container);
     });
   }
