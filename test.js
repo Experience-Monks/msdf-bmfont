3a128027dccd440bbd459a973ca38fc8b0a81c5f
Ari Lotter <ari.lotter@jam3.com>
2016-08-09 11:57:25 -0400

    renamed msdfgen.js to index.js, index -> test.js

---
+++ test.js
@@ -0,0 +1,22 @@
+const generateBMFont = require('./msdfgen');
+const fs = require('fs');
+const path = require('path');
+
+const opt = {
+  charset: 'ABCDEFG1234567 ',
+  fieldType: 'msdf'
+};
+generateBMFont('Roboto-Regular.ttf', opt, (error, textures, font) => {
+  if (error) throw error;
+  textures.forEach((sheet, index) => {
+    font.pages.push(`sheet${index}.png`);
+    fs.writeFile(path.join('output', `sheet${index}.png`), sheet, (err) => {
+      if (err) throw err;
+      console.log('wrote spritesheet', index);
+    });
+  });
+  fs.writeFile(path.join('output', 'font.fnt'), JSON.stringify(font), (err) => {
+    if (err) throw err;
+    console.log('wrote font file');
+  });
+});
