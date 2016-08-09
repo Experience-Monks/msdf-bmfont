db936941d655b6c7b616180d647ba27e12b06200
Ari Lotter <ari.lotter@jam3.com>
2016-08-09 11:47:07 -0400

    api changes

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-const getBMFontGenerator = require('./msdfgen');
+const generateBMFont = require('./msdfgen');
 const fs = require('fs');
 const path = require('path');
 
-const generateBMFont = getBMFontGenerator('Roboto-Regular.ttf');
-
-const charset = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz '.split('');
-
-const fontSize = 32;
-const pageWidth = 512;
-const pageHeight = 512;
-const pagePadding = 2;
-
-generateBMFont(charset, fontSize, pageWidth, pageHeight, pagePadding, (generated) => {
-  generated.spritesheets.forEach((sheet, index) => {
-    generated.fontData.pages.push(`sheet${index}.png`);
+const opt = {
+  charset: 'ABCDEFG1234567'.split(''),
+  fieldType: 'msdf'
+};
+generateBMFont('Roboto-Regular.ttf', opt, (error, textures, font) => {
+  if (error) throw error;
+  textures.forEach((sheet, index) => {
+    font.pages.push(`sheet${index}.png`);
     fs.writeFile(path.join('output', `sheet${index}.png`), sheet, (err) => {
       if (err) throw err;
       console.log('wrote spritesheet', index);
     });
   });
-  fs.writeFile(path.join('output', 'font.fnt'), JSON.stringify(generated.fontData), (err) => {
+  fs.writeFile(path.join('output', 'font.fnt'), JSON.stringify(font), (err) => {
     if (err) throw err;
     console.log('wrote font file');
   });
